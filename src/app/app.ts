f81e9c72df5d2ae6f438b174ed787a19b94c0cf5
Gajax <morbalegajanan6990@gmail.com>
2026-07-14 17:12:16 +0530

    My latest changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './core/layout/navbar/navbar';
 import { Footer } from './core/layout/footer/footer';
-import { Teachers } from './features/teachers/teachers';
 import { GrammerAssignment } from './features/grammer/grammer-assignment/grammer-assignment';
 import { Questions } from './features/grammer/questions/questions';
+import { CoursesBookTrainer } from './features/courses/courses-book-trainer/courses-book-trainer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Footer,Teachers,GrammerAssignment,Questions],
+  imports: [RouterOutlet,Navbar,Footer,GrammerAssignment,Questions,CoursesBookTrainer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
