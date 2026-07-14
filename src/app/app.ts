7e27e329f5055adea6b488a66ab174ac8f74553b
Omkar Chavan <chavanomi07@gmail.com>
2026-07-14 14:08:51 +0530

    creating navbar footer and teachers dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './core/layout/navbar/navbar';
+import { Footer } from './core/layout/footer/footer';
+import { Teachers } from './features/teachers/teachers';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Navbar,Footer,Teachers],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
