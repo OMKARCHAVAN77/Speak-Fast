7707bf1d777dd65ba9f445999a3cc7e6a6b08e0d
Harshal-Patil-13 <patilharshal0362@gmail.com>
2026-07-14 17:40:58 +0530

    speak fast create a home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { Footer } from './core/layout/footer/footer';
 import { GrammerAssignment } from './features/grammer/grammer-assignment/grammer-assignment';
 import { Questions } from './features/grammer/questions/questions';
 import { CoursesBookTrainer } from './features/courses/courses-book-trainer/courses-book-trainer';
+import { Home } from './features/dashboard/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Footer,GrammerAssignment,Questions,CoursesBookTrainer],
+  imports: [RouterOutlet,Navbar,Footer,GrammerAssignment,Questions,CoursesBookTrainer,Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
