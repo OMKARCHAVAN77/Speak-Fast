a5d754996745adce65f2ddf79e612359c3c679e4
Harshal-Patil-13 <patilharshal0362@gmail.com>
2026-07-14 16:04:16 +0530

    modified app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { Teachers } from './features/teachers/teachers';
 import { GrammerAssignment } from './features/grammer/grammer-assignment/grammer-assignment';
 import { Questions } from './features/grammer/questions/questions';
 import { Home } from './features/dashboard/home/home';
+import { CoursesBookTrainer } from './features/courses/courses-book-trainer/courses-book-trainer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Footer,Teachers,GrammerAssignment,Questions,Home],
+  imports: [RouterOutlet,Navbar,Footer,Teachers,GrammerAssignment,Questions,Home,CoursesBookTrainer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
