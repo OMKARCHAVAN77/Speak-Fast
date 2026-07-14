33f0bae8e199d4ddd8eb3d1fff7d19b86f2c3c94
Harshal-Patil-13 <patilharshal0362@gmail.com>
2026-07-14 15:07:19 +0530

    SpeakFast create a home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GrammerAssignment } from './features/grammer/grammer-assignment/grammer-assignment';
 import { Questions } from './features/grammer/questions/questions';
+import { Home } from './features/dashboard/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,GrammerAssignment,Questions],
+  imports: [RouterOutlet,GrammerAssignment,Questions,Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
