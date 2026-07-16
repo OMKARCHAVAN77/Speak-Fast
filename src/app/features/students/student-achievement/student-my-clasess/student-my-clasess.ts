5c385d890448f4521e81372ee7c6a502db04b165
Gajax <morbalegajanan6990@gmail.com>
2026-07-16 17:46:28 +0530

    add student dashbord

---
+++ src/app/features/students/student-achievement/student-my-clasess/student-my-clasess.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-my-clasess',
+  imports: [],
+  templateUrl: './student-my-clasess.html',
+  styleUrl: './student-my-clasess.css',
+})
+export class StudentMyClasess {
+
+}
