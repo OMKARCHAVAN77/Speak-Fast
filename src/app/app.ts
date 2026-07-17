ea31c73d737dd0a796a60568fba3a5acae2fd4e4
Omkar Chavan <chavanomi07@gmail.com>
2026-07-17 15:21:28 +0530

    add routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import { Navbar } from './core/layout/navbar/navbar';
 import { Footer } from './core/layout/footer/footer';
 import { StudentProcess } from './features/students/student-achievement/student-process/student-process';
 import { StudentAchievement } from './features/students/student-achievement/student-achievement';
+import { registerAppScopedDispatcher } from '@angular/core/primitives/event-dispatch';
 // import { Teachers } from './features/teachers/teachers';
 // import { GrammerAssignment } from './features/grammer/grammer-assignment/grammer-assignment';
 // import { Questions } from './features/grammer/questions/questions';
