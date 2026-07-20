514f1d6e72bf452fc0411f028b15561d5835737d
Prajwal-Suryawanshi <suryawanshiprajwal05@gmail.com>
2026-07-21 02:35:27 +0530

    forgot password page

---
+++ frontend/src/app/features/auth/forgot-password/link-sent/link-sent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-link-sent',
+  imports: [],
+  templateUrl: './link-sent.html',
+  styleUrl: './link-sent.css',
+})
+export class LinkSent {
+
+}
