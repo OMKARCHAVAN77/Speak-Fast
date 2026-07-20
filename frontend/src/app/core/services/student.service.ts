78eaf6ee23cbb5d361f87f7f3c7e5044b4f52af0
Gajax <morbalegajanan6990@gmail.com>
2026-07-21 02:18:02 +0530

    student registration api bindede

---
+++ frontend/src/app/core/services/student.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn:'root'
+})
+export class StudentService{
+
+    constructor(private http:HttpClient){}
+
+
+    addStudentApi(data:any){
+      return  this.http.post('http://localhost:5000/api/students/register',data)
+    }
+}
