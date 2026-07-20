2f94fbac08205cd04f63fc2b5b2032ea08edbceb
Prajwal-Suryawanshi <suryawanshiprajwal05@gmail.com>
2026-07-20 23:52:30 +0530

    login page update - 2

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -6,6 +6,7 @@ import { StudentProcess } from './features/students/student-achievement/student-
 import { StudentAchievement } from './features/students/student-achievement/student-achievement';
 import { registerAppScopedDispatcher } from '@angular/core/primitives/event-dispatch';
 import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 // import { Teachers } from './features/teachers/teachers';
 // import { GrammerAssignment } from './features/grammer/grammer-assignment/grammer-assignment';
 // import { Questions } from './features/grammer/questions/questions';
@@ -14,13 +15,35 @@ import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Footer,],
+  imports: [RouterOutlet,Navbar,Footer,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App  {
+export class App implements OnInit {
   protected readonly title = signal('frontend');
+  isNavbarOn: boolean= true;
+  hiddenRoutes = ['/', '/login'];
+  constructor(private actRout: ActivatedRoute, private route: Router){}
+
+  ngOnInit(): void {
+      console.log("router url is ",this.actRout.snapshot.url)
+      this.route.events.pipe(filter(x=>x instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+        //  this.isNavbarOn = !this.hiddenRoutes.includes(event.urlAfterRedirects);
+             if(event.url == "/" || event.url == "/login"){
+              this.isNavbarOn= false;
+              console.log(this.isNavbarOn);
+             }else{
+              this.isNavbarOn=true;
+              console.log(this.isNavbarOn);
+
+             }
 
 
+         }
+      )
+
+
+
+  }
 
 }
