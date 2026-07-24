01c5ad586037212513afeff3fcde20a0ee1678cc
Omkar Chavan <chavanomi07@gmail.com>
2026-07-24 11:22:23 +0530

    fix email sending on render

--- backend/src/config/mail.config.js
+++ backend/src/config/mail.config.js
@@ -4,28 +4,38 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
+
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
 
+  // Render IPv6 connection issue fix
+  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    pass: process.env.EMAIL_PASS,
   },
 
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+
   tls: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 
-// transporter.verify((error) => {
-//   if (error) {
-//     console.error("SMTP Error:", error.message);
-//   } else {
-//     console.log("✅ SMTP Connected Successfully");
-//   }
-// });
+transporter.verify((error) => {
+
+  if (error) {
+    console.error("❌ SMTP Error:", error.message);
+  } else {
+    console.log("✅ SMTP Connected Successfully");
+  }
+
+});
 
 
 export default transporter;
