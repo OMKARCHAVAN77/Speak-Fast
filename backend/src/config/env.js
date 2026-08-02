2e12df2346a82890a37287e4e21142097ebe2b6f
Omkar Chavan <chavanomi07@gmail.com>
2026-08-02 11:42:12 +0530

    change in app.js backend

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,22 +1,3 @@
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const env = {
-//   PORT: process.env.PORT,
-//   NODE_ENV: process.env.NODE_ENV,
-
-//   WHATSAPP_API_VERSION: process.env.WHATSAPP_API_VERSION,
-//   WHATSAPP_ACCESS_TOKEN: process.env.WHATSAPP_ACCESS_TOKEN,
-//   WHATSAPP_PHONE_NUMBER_ID: process.env.WHATSAPP_PHONE_NUMBER_ID,
-//   WHATSAPP_BUSINESS_ACCOUNT_ID: process.env.WHATSAPP_BUSINESS_ACCOUNT_ID,
-//   WHATSAPP_VERIFY_TOKEN: process.env.WHATSAPP_VERIFY_TOKEN,
-
-//   TEACHER_WHATSAPP_NUMBER: process.env.TEACHER_WHATSAPP_NUMBER,
-// };
-
-// export default env;
-
 
 import dotenv from "dotenv";
 
