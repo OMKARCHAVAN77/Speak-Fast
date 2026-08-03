614b2282959b548e6af939e2543595ae7d0ee733
Harshal-Patil-13 <patilharshal0362@gmail.com>
2026-08-03 15:00:30 +0530

    scroll top issue fix

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -10,7 +10,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes,),
+    provideRouter(routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',   
+        anchorScrolling: 'enabled'
+      })
+    ),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     provideAnimations(),
    provideToastr({
